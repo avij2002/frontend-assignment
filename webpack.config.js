96dece41d088df67e55549301fda9ecf2fc0e1af
Aayush <aayushvij2002@gmail.com>
2025-01-20 22:36:16 +0530

    Commiting build

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,6 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
-  mode: "development",
-  devtool: "inline-source-map",
   module: {
     rules: [
       {
